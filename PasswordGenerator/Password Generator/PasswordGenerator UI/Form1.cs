154cd5cd13128b88aa75e593e5a9899df971abae
VLAD <Cumpatescuvlad@yaho.com>
2022-09-17 03:17:36 +0300

    UPDATED

---
+++ PasswordGenerator/Password Generator/PasswordGenerator UI/Form1.cs
@@ -0,0 +1,40 @@
+using PasswordGenerator_2;
+
+namespace PasswordGenerator_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Generate generate = new Generate();
+
+            textBox1.Text = $"PASSWORD IS : {generate.GenerateAndStorePassword(@"E:\CODE REPOS\PasswordGenerator\Password Generator\Password.txt")}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text = File.ReadAllText(@"E:\CODE REPOS\PasswordGenerator\Password Generator\Password.txt");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
